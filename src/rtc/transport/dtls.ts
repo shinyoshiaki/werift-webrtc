7e530aee194c2011bf908c9e1f146679e1454087
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-02-17 16:39:25 +0900

    add rfc

---
+++ src/rtc/transport/dtls.ts
@@ -0,0 +1,11 @@
+import { RTCIceTransport } from "./ice";
+
+export class RTCDtlsTransport {
+  constructor(transport: RTCIceTransport) {}
+}
+
+export class RTCCertificate {
+  constructor() {}
+
+  static generateCertificate() {}
+}
