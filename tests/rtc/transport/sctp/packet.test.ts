a2ae45393b4e92cfd3f0a94f1612cd022286162f
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-03-17 15:50:38 +0900

    test test_parse_init_invalid_checksum

--- tests/rtc/transport/sctp/packet.test.ts
+++ tests/rtc/transport/sctp/packet.test.ts
@@ -32,4 +32,18 @@ describe("SctpPacketTest", () => {
     expect(chunk.flags).toBe(0);
     expect(chunk.body!.length).toBe(82);
   });
+
+  test("test_parse_init_invalid_checksum", () => {
+    let data = load("sctp_init.bin");
+    data = Buffer.concat([
+      data.slice(0, 8),
+      Buffer.from("\x01\x02\x03\x04"),
+      data.slice(12)
+    ]);
+    try {
+      roundtripPacket(data);
+    } catch (error) {
+      expect(error.message).toBe("SCTP packet has invalid checksum");
+    }
+  });
 });
