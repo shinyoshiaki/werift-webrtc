62561f02b713afd9f0199bce516fddbc7d7aa1e7
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-06-09 01:14:50 +0900

    fix sdp

--- src/rtc/peerConnection.ts
+++ src/rtc/peerConnection.ts
@@ -84,9 +84,9 @@ export class RTCPeerConnection {
       );
 
     const mids = [...this.seenMid];
-    const ntpSeconds = Date.now() >> 32;
+    const ntpSeconds = (Date.now() / 1000) >> 32;
     const description = new SessionDescription();
-    description.origin = `${ntpSeconds} ${ntpSeconds} IN IP4 0.0.0.0`;
+    description.origin = `- ${ntpSeconds} ${ntpSeconds} IN IP4 0.0.0.0`;
     description.msidSemantic.push(new GroupDescription("WMS", ["*"]));
     description.type = "offer";
 
