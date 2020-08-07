0e354b1e7f1d4ea15bfedab0e85e95987ceb1c0c
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-08-07 12:14:18 +0900

    change dir examples

---
+++ examples/mediachannel/recvonly/offer.ts
@@ -0,0 +1,29 @@
+import { RTCPeerConnection } from "../../../src";
+import { Server } from "ws";
+import { createSocket } from "dgram";
+
+const server = new Server({ port: 8888 });
+console.log("start");
+const udp = createSocket("udp4");
+
+server.on("connection", async (socket) => {
+  const pc = new RTCPeerConnection({
+    stunServer: ["stun.l.google.com", 19302],
+  });
+  pc.iceConnectionStateChange.subscribe((v) =>
+    console.log("pc.iceConnectionStateChange", v)
+  );
+  const transceiver = pc.addTransceiver("video", "recvonly");
+  transceiver.receiver.onRtp.subscribe((packet) => {
+    udp.send(packet.serialize(), 4002, "127.0.0.1");
+  });
+
+  const offer = pc.createOffer();
+  await pc.setLocalDescription(offer);
+  const sdp = JSON.stringify(pc.localDescription);
+  socket.send(sdp);
+
+  socket.on("message", (data: any) => {
+    pc.setRemoteDescription(JSON.parse(data));
+  });
+});
--- examples/mediachannel/sendonly/offer.ts
+++ examples/mediachannel/sendonly/offer.ts
@@ -1,10 +1,11 @@
-import { RTCPeerConnection } from "../../src";
+import { RTCPeerConnection } from "../../../src";
 import { Server } from "ws";
 import { createSocket } from "dgram";
 
 const server = new Server({ port: 8888 });
 console.log("start");
 const udp = createSocket("udp4");
+udp.bind(5000);
 
 server.on("connection", async (socket) => {
   const pc = new RTCPeerConnection({
@@ -13,10 +14,7 @@ server.on("connection", async (socket) => {
   pc.iceConnectionStateChange.subscribe((v) =>
     console.log("pc.iceConnectionStateChange", v)
   );
-  const transceiver = pc.addTransceiver("video", "recvonly");
-  transceiver.receiver.onRtp.subscribe((packet) => {
-    udp.send(packet.serialize(), 4002, "127.0.0.1");
-  });
+  const transceiver = pc.addTransceiver("video", "sendonly");
 
   const offer = pc.createOffer();
   await pc.setLocalDescription(offer);
@@ -25,5 +23,16 @@ server.on("connection", async (socket) => {
 
   socket.on("message", (data: any) => {
     pc.setRemoteDescription(JSON.parse(data));
+  });
+
+  await new Promise((r) => {
+    pc.iceConnectionStateChange.subscribe((state) => {
+      if (state === "completed") {
+        r();
+      }
+    });
+  });
+  udp.on("message", (data) => {
+    transceiver.sender.sendRtp(data);
   });
 });
