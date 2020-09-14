26e05f6848aa10b0e26cb2b7127937321ed07a2d
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-09-14 19:17:33 +0900

    ver up

--- src/helper.ts
+++ src/helper.ts
@@ -1,20 +1,9 @@
-import Event from "rx.mini";
-
 export function enumerate<T>(arr: T[]): [number, T][] {
   return arr.map((v, i) => [i, v]);
 }
 
 export async function sleep(ms: number) {
   await new Promise((r) => setTimeout(r, ms));
-}
-
-export type Unpacked<T> = T extends { [K in keyof T]: infer U } ? U : never;
-
-export function createEventsFromList(list: any) {
-  return list.reduce((acc, cur) => {
-    acc[cur] = new Event();
-    return acc;
-  }, {} as any);
 }
 
 export function divide(from: string, split: string): [string, string] {
