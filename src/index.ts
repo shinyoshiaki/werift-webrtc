e62dfd21018def4c11f51245787c106289f7360f
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-09-14 18:40:28 +0900

    export something

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-export { Direction } from "./rtc/media/rtpTransceiver";
-
+export { RtpTrack } from "./rtc/media/track";
+export { RTCDataChannel } from "./rtc/dataChannel";
+export { RTCSessionDescription } from "./rtc/sdp";
+export { useSdesMid, useSdesRTPStreamID } from "./rtc/extension/rtpExtension";
+export { Direction, RTCRtpTransceiver } from "./rtc/media/rtpTransceiver";
 export { RTCRtpCodecParameters } from "./rtc/media/parameters";
-
 export { RTCCertificate } from "./rtc/transport/dtls";
-
 export { RTCPeerConnection, PeerConfig } from "./rtc/peerConnection";
-
 export { RTCSctpTransport } from "./rtc/transport/sctp";
-
 export { RTCIceGatherer, RTCIceTransport } from "./rtc/transport/ice";
-
 export { IceOptions } from "./vendor/ice";
