f46fa393b8b67caa94550a58694626649deb78a1
shinyoshiaki <shinyoshiaki2012@gmail.com>
2021-04-28 19:45:29 +0900

    doc: update example

--- examples/interop/client.ts
+++ examples/interop/client.ts
@@ -3,21 +3,34 @@ import {
   RTCPeerConnection,
   RtpPacket,
 } from "../../packages/webrtc/src";
-import axios from "axios";
+import got from "got";
+import * as yargs from "yargs";
 import { createSocket } from "dgram";
 import { exec } from "child_process";
-import debug from "debug";
 
-const log = debug("werift/interop");
+const TestType = { PeerConnection: 0, DataChannelEcho: 1 };
 
-const url = process.argv[2] || "http://localhost:8080/offer";
+const args = yargs
+  .option("s", {
+    default:
+      process.argv.length === 3
+        ? process.argv[2] || "http://localhost:8080/offer"
+        : "http://localhost:8080/offer",
+  })
+  .option("t", {
+    default: TestType.PeerConnection,
+  })
+  .help().argv;
+const url = args["s"];
+const testType = args["t"];
+console.log(args, { url, testType });
 
 const udp = createSocket("udp4");
 udp.bind(5000);
 
-new Promise<void>(async (r, f) => {
+new Promise<void>(async (done, failed) => {
   setTimeout(() => {
-    f();
+    failed();
   }, 60_000);
 
   const pc = new RTCPeerConnection({
@@ -27,46 +40,70 @@ new Promise<void>(async (r, f) => {
   const transceiver = pc.addTransceiver(senderTrack);
   transceiver.onTrack.once((track) => {
     track.onReceiveRtp.subscribe((rtp) => {
-      log(rtp.header);
-      log("Received echoed back rtp");
-      r();
+      console.log("Received echoed back rtp", rtp.header);
+      if (testType === TestType.PeerConnection) {
+        done();
+      }
     });
   });
 
-  await pc.setLocalDescription(await pc.createOffer()).catch((e) => f(e));
-  const { data } = await axios.post(url, pc.localDescription).catch((e) => {
-    f(e);
-    throw e;
-  });
-  log("server answer sdp", data?.sdp);
-  pc.setRemoteDescription(data).catch((e) => f(e));
+  if (testType === TestType.DataChannelEcho) {
+    const pseudo = Math.random().toString().slice(2, 7);
+    const dc = pc.createDataChannel("werift-dc");
+    dc.onopen = () => {
+      dc.send(pseudo);
+    };
+    dc.message.subscribe((msg) => {
+      console.log("data channel onmessage");
+      if (msg === pseudo) {
+        console.log("Data channel echo test success.");
+        if (testType === TestType.DataChannelEcho) {
+          done();
+        }
+      }
+    });
+  }
+
+  await pc.setLocalDescription(await pc.createOffer());
+  console.log("local offer ", pc.localDescription?.sdp);
+  const data = await got
+    .post(url, {
+      json: pc.localDescription,
+      retry: { limit: 5, methods: ["POST"] },
+    })
+    .json<any>()
+    .catch(failed);
+  console.log("server answer sdp", data?.sdp);
+  pc.setRemoteDescription(data);
 
   await pc.connectionStateChange.watch((state) => state === "connected");
   if (transceiver.receiver.tracks.length === 0) {
-    log("remoteTrack not found");
-    r();
+    console.log("remoteTrack not found");
+    done();
     return;
   }
 
-  const payloadType = transceiver.codecs.find((codec) =>
-    codec.mimeType.toLowerCase().includes("vp8")
-  )!.payloadType;
+  if (testType === TestType.PeerConnection) {
+    const payloadType = transceiver.codecs.find((codec) =>
+      codec.mimeType.toLowerCase().includes("vp8")
+    )!.payloadType;
 
-  udp.on("message", (data) => {
-    const rtp = RtpPacket.deSerialize(data);
-    rtp.header.payloadType = payloadType;
-    senderTrack.writeRtp(rtp);
-  });
+    udp.on("message", (data) => {
+      const rtp = RtpPacket.deSerialize(data);
+      rtp.header.payloadType = payloadType;
+      senderTrack.writeRtp(rtp);
+    });
 
-  exec(
-    "gst-launch-1.0 videotestsrc ! video/x-raw,width=640,height=480,format=I420 ! vp8enc error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true cpu-used=5 deadline=1 ! rtpvp8pay ! udpsink host=127.0.0.1 port=5000"
-  );
+    exec(
+      "gst-launch-1.0 videotestsrc ! video/x-raw,width=640,height=480,format=I420 ! vp8enc error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true cpu-used=5 deadline=1 ! rtpvp8pay ! udpsink host=127.0.0.1 port=5000"
+    );
+  }
 })
   .then(() => {
-    log("done");
+    console.log("done");
     process.exit(0);
   })
   .catch((e) => {
-    log("failed", e);
+    console.log("failed", e);
     process.exit(1);
   });
