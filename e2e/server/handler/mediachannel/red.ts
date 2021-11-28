e1c98b17821a1af8497fb5e18aff735453f65a31
shinyoshiaki <shinyoshiaki2012@gmail.com>
2021-11-29 00:11:27 +0900

    add test

--- e2e/server/handler/mediachannel/red.ts
+++ e2e/server/handler/mediachannel/red.ts
@@ -78,3 +78,75 @@ export class mediachannel_red_client_answer {
     }
   }
 }
+
+export class mediachannel_red_client_offer {
+  pc!: RTCPeerConnection;
+  process!: ChildProcess;
+  udp = createSocket("udp4");
+
+  async exec(type: string, payload: any, accept: AcceptFn) {
+    switch (type) {
+      case "init":
+        {
+          this.pc = new RTCPeerConnection({
+            iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+            codecs: {
+              audio: [
+                new RTCRtpCodecParameters({
+                  mimeType: "audio/red",
+                  clockRate: 48000,
+                  channels: 2,
+                }),
+                new RTCRtpCodecParameters({
+                  mimeType: "audio/opus",
+                  clockRate: 48000,
+                  channels: 2,
+                }),
+              ],
+            },
+          });
+          accept({});
+        }
+        break;
+      case "offer":
+        {
+          const port = await randomPort();
+          this.udp.bind(port);
+
+          const track = new MediaStreamTrack({ kind: "audio" });
+          this.pc.addTrack(track);
+
+          await this.pc.setRemoteDescription(payload);
+          await this.pc.setLocalDescription(await this.pc.createAnswer());
+          accept(this.pc.localDescription);
+
+          this.udp.on("message", (data) => {
+            track.writeRtp(data);
+          });
+
+          const args = [
+            `audiotestsrc wave=ticks ! audioconvert ! audioresample ! queue ! opusenc ! rtpopuspay`,
+            `udpsink host=127.0.0.1 port=${port}`,
+          ].join(" ! ");
+          this.process = spawn("gst-launch-1.0", args.split(" "));
+        }
+        break;
+      case "candidate":
+        {
+          await this.pc.addIceCandidate(payload);
+          accept({});
+        }
+        break;
+      case "done":
+        {
+          this.udp.close();
+          try {
+            this.process.kill("SIGINT");
+          } catch (error) {}
+          this.pc.close();
+          accept({});
+        }
+        break;
+    }
+  }
+}
