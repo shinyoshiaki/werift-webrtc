5f160ecdc7978e98e0cad08c9c1fce187bba5714
shinyoshiaki <shinyoshiaki2012@gmail.com>
2023-11-09 20:52:24 +0900

    parse h264 parameter

--- packages/webrtc/tests/sdp.test.ts
+++ packages/webrtc/tests/sdp.test.ts
@@ -1,4 +1,9 @@
-import { GroupDescription, MediaDescription, SessionDescription } from "../src";
+import {
+  codecParametersFromString,
+  GroupDescription,
+  MediaDescription,
+  SessionDescription,
+} from "../src";
 
 describe("sdp", () => {
   test("rtx", () => {
@@ -11,5 +16,18 @@ describe("sdp", () => {
     expect(SessionDescription.parse(str).media[0].ssrcGroup).toEqual([
       new GroupDescription("FID", ["ssrc", "rtx"]),
     ]);
+  });
+
+  describe("codecParametersFromString", () => {
+    test("h264 parameters", () => {
+      const params = codecParametersFromString(
+        "packetization-mode:0;profile-level-id:42001f;level-asymmetry-allowed:0"
+      );
+      expect(params).toEqual({
+        "level-asymmetry-allowed": 0,
+        "packetization-mode": 0,
+        "profile-level-id": "42001f",
+      });
+    });
   });
 });
